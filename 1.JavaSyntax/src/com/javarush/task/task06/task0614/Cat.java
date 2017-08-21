902be1fce69efc15fbf4bf70bfcd872cca178612
ActualAlex <actual.mebel@gmail.com>
2017-08-21 16:57:19 +0300

    i

--- 1.JavaSyntax/src/com/javarush/task/task06/task0614/Cat.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0614/Cat.java
@@ -7,17 +7,22 @@ import java.util.ArrayList;
 */
 
 public class Cat {
-    //напишите тут ваш код
+    public static ArrayList<Cat> cats = new ArrayList<Cat>();//напишите тут ваш код
 
     public Cat() {
     }
 
     public static void main(String[] args) {
-        //напишите тут ваш код
+        for (int i = 0; i < 10; i++) {
+            Cat cat = new Cat();
+            cats.add(cat);//напишите тут ваш код
+        }
         printCats();
     }
 
     public static void printCats() {
-        //напишите тут ваш код
+        for (int i = 0; i < cats.size(); i++) {
+            System.out.println(cats.get(i));
+        }//напишите тут ваш код
     }
 }
