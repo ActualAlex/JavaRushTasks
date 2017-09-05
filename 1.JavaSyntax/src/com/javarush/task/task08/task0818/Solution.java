5a1216db58531b7401fcf2533277168e846b4cf5
ActualAlex <actual.mebel@gmail.com>
2017-09-05 15:39:26 +0300

    i

--- 1.JavaSyntax/src/com/javarush/task/task08/task0818/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0818/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task08.task0818;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,11 +10,30 @@ import java.util.Map;
 
 public class Solution {
     public static HashMap<String, Integer> createMap() {
-        //напишите тут ваш код
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("Первый", 600);//напишите тут ваш код
+        map.put("Второй", 700);//напишите тут ваш код
+        map.put("Третий", 450);//напишите тут ваш код
+        map.put("Четвертый", 120);//напишите тут ваш код
+        map.put("Пятый", 400);//напишите тут ваш код
+        map.put("Шестой", 130);//напишите тут ваш код
+        map.put("Седьмой", 330);//напишите тут ваш код
+        map.put("Восьмой", 770);//напишите тут ваш код
+        map.put("Девятый", 6000);//напишите тут ваш код
+        map.put("Десятый", 6020);//напишите тут ваш код
+        return map;
     }
 
     public static void removeItemFromMap(HashMap<String, Integer> map) {
-        //напишите тут ваш код
+        ArrayList<String> list = new ArrayList<>();
+        for (Map.Entry<String, Integer> m : map.entrySet()) {
+            if (m.getValue() < 500) {
+                list.add(m.getKey());//напишите тут ваш код
+            }
+        }
+        for (String s : list) {
+            if (map.containsKey(s)) map.remove(s);
+        }
     }
 
     public static void main(String[] args) {
